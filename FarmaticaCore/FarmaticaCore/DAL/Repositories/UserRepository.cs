ccffcf48444581ba0f5a413f9924a5137fce7bd5
manu3193 <manu3193@gmail.com>
2016-03-25 10:49:27 -0600

    Implemented and tested all user repository CRUD methods

--- FarmaticaCore/FarmaticaCore/DAL/Repositories/UserRepository.cs
+++ FarmaticaCore/FarmaticaCore/DAL/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Runtime.InteropServices;
 using FarmaticaCore.DAL.Models;
 
@@ -21,7 +22,7 @@ namespace FarmaticaCore.DAL.Repositories
         /// CRUD method to insert a new user
         /// </summary>
         /// <param name="user">user to insert</param>
-        public void Create(User user)
+        public override void Create(User user)
         {
             using (var command = Context.CreateDbCommand())
             {
@@ -39,15 +40,93 @@ namespace FarmaticaCore.DAL.Repositories
                 command.ExecuteNonQuery();
             }
         }
+
+        /// <summary>
+        /// Get all users in the database
+        /// </summary>
+        /// <returns>A list of Users</returns>
+        public override IEnumerable<User> GetAll()
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                command.CommandText = @"SELECT * FROM Usuario";
+                var result = ToList(command);
+                return result;
+            }
+        } 
+        
+        /// <summary>
+        /// Get all users with the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Users</User></returns>
+        public override IEnumerable<User> GetById(object id)
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                command.CommandText = @"SELECT * FROM Usuario WHERE ID_Usuario = @userId";
+                var newParameter = command.CreateParameter();
+                newParameter.ParameterName = "@userId";
+                newParameter.Value = id;
+                command.Parameters.Add(newParameter);
+                var result = ToList(command);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Updates an existing user
+        /// </summary>
+        /// <param name="user"></param>
+        public override void Update(User user)
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                var userProps = new object[]
+                { user.Password, user.Name, user.LastName1, user.LastName2, user.Email, user.Role};
+                command.CommandText = @"UPDATE Usuario SET Pass= @pass, Nombre= @name, PrimerApellido= @lastName1, SegundoApellido= @lastName2, Email= @email, Rol_Usuario= @role WHERE Id_Usuario=@userId";
+                var parameterNames = new string[] {"@pass", "@name", "@lastName1", "@lastName2", "@email", "@role" };
+                for (var i = 0; i < userProps.Length; i++)
+                {
+                    var newParameter = command.CreateParameter();
+                    newParameter.ParameterName = parameterNames[i];
+                    newParameter.Value = userProps[i];
+                    command.Parameters.Add(newParameter);
+                }
+                var idParameter = command.CreateParameter();
+                idParameter.ParameterName = "@userId";
+                idParameter.Value = user.IdUsuario;
+                command.Parameters.Add(idParameter);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Delete an existing user from repository
+        /// </summary>
+        /// <param name="id">user id</param>
+        public override void DeleteById(object id)
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                command.CommandText = @"DELETE FROM Usuario WHERE ID_Usuario= @userId";
+                var newParameter = command.CreateParameter();
+                newParameter.ParameterName = "@userId";
+                newParameter.Value = id;
+                command.Parameters.Add(newParameter);
+                command.ExecuteNonQuery();
+            }
+        }
+
         protected override void Map(IDataRecord record, User entity)
         {
-            entity.IdUsuario = (string) record["ID_Usuario"];
-            entity.Password = (string) record["Pass"];
-            entity.Name = (string) record["Nombre"];
-            entity.LastName1 = (string) record["PrimerApellido"];
-            entity.LastName2 = (string) record["SegundoApellido"];
-            entity.Email = (string) record["Email"];
-            entity.Role = (int) record["Rol_Usuario"];
+            entity.IdUsuario = (string)record["ID_Usuario"];
+            entity.Password = (string)record["Pass"];
+            entity.Name = (string)record["Nombre"];
+            entity.LastName1 = (string)record["PrimerApellido"];
+            entity.LastName2 = (string)record["SegundoApellido"];
+            entity.Email = (string)record["Email"];
+            entity.Role = (int)record["Rol_Usuario"];
         }
     }
 }
