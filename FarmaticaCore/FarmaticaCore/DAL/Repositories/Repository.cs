1e787871a1d8ddb54984b2cc2259e3fa2fd90034
manu3193 <manu3193@gmail.com>
2016-03-25 03:37:34 -0600

    Terminadas las interfaces e implementaciones de la capa de datos. Se ha creado el repositorio de usuario y su metodo Crear(User user). Falta implementar los otros repositorios,

---
+++ FarmaticaCore/FarmaticaCore/DAL/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace FarmaticaCore.DAL.Repositories
+{
+    /// <summary>
+    /// Abstract Repository class to be implemented in specific repository type
+    /// </summary>
+    /// <typeparam name="TEntity">Entity</typeparam>
+    public abstract class Repository<TEntity> where TEntity : new()
+    {
+        DbContext mDbContext;
+
+        /// <summary>
+        /// Intitialize the repository
+        /// </summary>
+        /// <param name="context"></param>
+        public Repository(DbContext context)
+        {
+            mDbContext = context;
+            Context = context;
+        }
+
+        protected DbContext Context { get;}
+
+
+        protected IEnumerable<TEntity> ToList(IDbCommand command)
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                List<TEntity> itemList = new List<TEntity>();
+                while (reader.Read())
+                {
+                    var item = new TEntity();
+                    Map(reader, item);
+                    itemList.Add(item);
+                }
+                return itemList;
+            } 
+        }
+
+        protected abstract void Map(IDataRecord record, TEntity entity);
+    }
+}
