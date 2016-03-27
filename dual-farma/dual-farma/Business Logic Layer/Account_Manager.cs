33fea698b78b480c8fd59c51c704058f2402f31a
kevuo <kevgiso@hotmail.com>
2016-03-27 16:28:11 -0600

    Create Account Manager class in BLL.

---
+++ dual-farma/dual-farma/Business Logic Layer/Account_Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dual_farma.Business_Logic_Layer
+{
+    public class Account_Manager
+    {
+        private 
+    public Account_Manager()
+        {
+
+        }
+    }
+}
