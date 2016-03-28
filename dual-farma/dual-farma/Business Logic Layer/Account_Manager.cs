0a785682dfb55a8c24350c5a64005f97745f79e6
kevuo <kevgiso@hotmail.com>
2016-03-27 19:16:33 -0600

    Set attributes to Account Manager.

--- dual-farma/dual-farma/Business Logic Layer/Account_Manager.cs
+++ dual-farma/dual-farma/Business Logic Layer/Account_Manager.cs
@@ -1,19 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace dual_farma.Business_Logic_Layer
+﻿namespace dual_farma.Business_Logic_Layer
 {
+
     public class Account_Manager
     {
         private string userID;
         private string password;
+        var factory = new DbConnectionFactory("local");
+        var context = new DbContext(factory);
+        public string UserID
+        {
+            get
+            {
+                return userID;
+            }
+
+            set
+            {
+                userID = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
 
         public Account_Manager(string userID, string password)
         {
             userID = this.userID;
             password = this.password;
         }
+
+        public Account_Manager()
+        { }
+
+        private int AuthorizeLogin(string userID, string password){
+            if
+            
+            }
+       
+       
+
     }
 }
