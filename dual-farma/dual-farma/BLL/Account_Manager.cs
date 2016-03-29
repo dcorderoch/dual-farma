ec9ae4a95e7b0efc3094f8cf85ff6a11cd19bfe8
kevuo <kevgiso@hotmail.com>
2016-03-29 00:15:51 -0600

    Account_Manager: Authorize Login method completed. Moving on to Create User method.

--- dual-farma/dual-farma/BLL/Account_Manager.cs
+++ dual-farma/dual-farma/BLL/Account_Manager.cs
@@ -1,53 +1,116 @@
-﻿namespace dual_farma.BLL
-{
+﻿using System;
+using FarmaticaCore.DAL;
+using FarmaticaCore.DAL.Repositories;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FarmaticaCore.DAL.Models;
 
+namespace dual_farma.BLL
+{
+    /// <summary>
+    /// Account_Manager is intended to validate most of the business rules related to the Users' login. 
+    /// </summary>
     public class Account_Manager
     {
-        private string userID;
-        private string password;
-        var factory = new DbConnectionFactory("local");
-        var context = new DbContext(factory);
-        public string UserID
-        {
-            get
-            {
-                return userID;
-            }
+        /// <summary>
+        /// Class members which allow connecting to the Database.
+        /// </summary>
+        private DbConnectionFactory factory;
 
-            set
-            {
-                userID = value;
-            }
-        }
-
-        public string Password
-        {
-            get
-            {
-                return password;
-            }
-
-            set
-            {
-                password = value;
-            }
-        }
-
-        public Account_Manager(string userID, string password)
-        {
-            userID = this.userID;
-            password = this.password;
-        }
+        private DbContext context;
 
         public Account_Manager()
-        { }
+        {
+            factory = new DbConnectionFactory("Azure");
+            context = new DbContext(factory);
+        }
 
-        private int AuthorizeLogin(string userID, string password){
-            if
-            
+        /// <summary>
+        /// Method that communicates directly to the REST API, providing it with the User's details in case the login results succesful. Otherwise an error code
+        /// is returned within an Array List of strings.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string[] AuthorizeLogin(string userID, string password)
+        {
+            string[] response = {};
+            using (var uow = context.CreateUnitOfWork())
+            {
+                var userRepo = new UserRepository(context);
+                var userList = (List<User>) userRepo.GetById(userID);
+                var user = userList[0];
+                var opCode = VerifyUser(userID, password);
+                switch (opCode)
+                {
+                    case Constants.INVALID_USER:
+                        response[0] = Constants.INVALID_USER.ToString();
+                        return response;
+
+                    case Constants.INVALID_PASSWORD:
+                        response[0] = Constants.INVALID_PASSWORD.ToString();
+                        return response;
+
+                    case Constants.SUCCESSFUL_LOGIN:
+                        response[0] = Constants.SUCCESSFUL_LOGIN.ToString();
+                        response[1] = user.IdUsuario;
+                        response[2] = user.Name;
+                        response[3] = user.LastName1;
+                        response[4] = user.LastName2;
+                        response[5] = user.Email;
+                        response[6] = user.RoleId.ToString();
+                        return response;
+                    default:
+                        response[0] = Constants.INVALID_USER.ToString();
+                        return response;
+                }
             }
-       
-       
+        }
 
-    }
+        public int CreateUser(string[] user)
+        {
+            int response = 0;
+            using (var uow = context.CreateUnitOfWork())
+            {
+                var userRepo = new UserRepository(context);
+                var userList = (List<User>)userRepo.GetAll();
+                bool doesUserExist;
+                string userId = user[0];
+
+
+
+
+
+            }
+        }
+
+        /// <summary>
+        /// Verifies the existence of a User given its ID and Password
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="password"></param>
+        /// <returns>Operation code that indicates whether it exists in the database or not, or the password given is incorrect.</returns>
+        private int VerifyUser(string userID, string password)
+        {
+            var result = 0;
+
+            using (var uow = context.CreateUnitOfWork())
+            {
+                var userRepo = new UserRepository(context);
+                var userList = (List<User>) userRepo.GetById(userID);
+
+                if (userList.Count == 0)
+                {
+                    result = Constants.INVALID_USER;
+                }
+                else
+                {
+                    result = userList[0].Password != password ? Constants.INVALID_PASSWORD : Constants.SUCCESSFUL_LOGIN;
+                }
+            }
+            return result;
+        }
+    
+      }
 }
---
+++ dual-farma/dual-farma/BLL/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace dual_farma.BLL
+{
+    /// <summary>
+    /// Constants which determine the message displayed by the GUI after trying to log in to the application.
+    /// </summary>
+    public static class Constants
+    {
+        public const int INVALID_USER = 0;
+        public const int INVALID_PASSWORD= 1;
+        public const int SUCCESSFUL_LOGIN = 2;
+        public const int ALREADY_REGISTERED = 0;
+        public const int USER_CREATED = 1;
+
+    }
+}
