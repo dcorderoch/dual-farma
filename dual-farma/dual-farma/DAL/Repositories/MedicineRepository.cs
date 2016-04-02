1c54134852632dc159ffc3dec1c53006ed7acd1d
manu3193 <manu3193@gmail.com>
2016-04-02 01:41:27 -0600

    Fixed statistics and getbycompany methods

--- dual-farma/dual-farma/DAL/Repositories/MedicineRepository.cs
+++ dual-farma/dual-farma/DAL/Repositories/MedicineRepository.cs
@@ -28,6 +28,8 @@ namespace dual_farma.DAL.Repositories
             }
         }
 
+
+
         /// <summary>
         /// Creates a new medicine 
         /// </summary>
@@ -37,12 +39,9 @@ namespace dual_farma.DAL.Repositories
             using (var command = Context.CreateDbCommand())
             {
                 var medicineProps = new object[]
-                {medicine.MedicineId.ToString(), medicine.Name, medicine.RequiresPrescription, medicine.Price, medicine.OriginOffice, medicine.House,
-                 medicine.Stock, medicine.NumberSold};
-                command.CommandText = @"INSERT INTO Medicamento VALUES(@medicineId, @name, @reqPresc, @price, @originOffice, @house, " +
-                                                                      "@stock, @numberSold)";
-                var parameterNames = new string[] {"@medicineId", "@name", "@reqPresc", "@price", "@originOffice", "@house",
-                                                    "@stock", "@numberSold"};
+                {medicine.MedicineId.ToString(), medicine.Name, medicine.RequiresPrescription};
+                command.CommandText = @"INSERT INTO Medicamento VALUES(@medicineId, @name, @reqPresc)";
+                var parameterNames = new string[] {"@medicineId", "@name", "@reqPresc"};
                 for (var i = 0; i < medicineProps.Length; i++)
                 {
                     var newParameter = command.CreateParameter();
@@ -73,35 +72,75 @@ namespace dual_farma.DAL.Repositories
             }
         }
 
-        /// <summary>
-        /// Get a list of the total most sold medicines
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<Medicine> GetTotalMostSold()
+        public IEnumerable<Medicine> GetAllByBranchOffice(Guid branchOfficeId)
         {
             using (var command = Context.CreateDbCommand())
             {
-                command.CommandText = @"SELECT * FROM Medicamento ORDER BY CantidadVentas DESC";
+                command.CommandText = @"SELECT M.ID_Medicamento,M.Nombre,M.Prescripcion,MPS.CantidadDisponible,MPS.CantidadVentas,MPS.Precio FROM "+
+                                       "Medicamento AS M JOIN Medicamentos_Por_Sucursal AS MPS ON M.ID_Medicamento= MPS.ID_Medicamento WHERE ID_Sucursal = @branchOfficeId";
                 var newParameter = command.CreateParameter();
-                var result = ToList(command);
+                newParameter.ParameterName = "@branchOfficeId";
+                newParameter.Value = branchOfficeId.ToString();
+                command.Parameters.Add(newParameter);
+                var result = ToCustomMedicineList(command);
                 return result;
             }
         }
 
         /// <summary>
-        /// Get a list of the total most sold medicines for the given company
+        /// Get a list of the total most sold medicines by company
         /// </summary>
         /// <returns></returns>
         public IEnumerable<Medicine> GetTotalMostSoldByCompany(string company)
         {
             using (var command = Context.CreateDbCommand())
             {
-                command.CommandText = @"SELECT * FROM Medicamento WHERE CasaFarmaceutica=@casaFarma ORDER BY CantidadVentas DESC";
+                command.CommandText = @"SELECT M.ID_Medicamento,M.Nombre,SUM(MPS.CantidadVentas) AS CantidadVentas "+
+                                       "FROM Medicamento M  JOIN Medicamentos_Por_Sucursal MPS ON M.ID_Medicamento= MPS.ID_Medicamento JOIN Sucursal S ON MPS.ID_Sucursal=S.ID_Sucursal "+
+                                       "WHERE S.Compañia = @company GROUP BY M.ID_Medicamento,M.Nombre ORDER BY CantidadVentas DESC";
                 var newParameter = command.CreateParameter();
-                newParameter.ParameterName = "@casaFarma";
+                newParameter.ParameterName = "@company";
                 newParameter.Value = company;
                 command.Parameters.Add(newParameter);
-                var result = ToList(command);
+                var result = ToStatisticMedicineList(command);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets total most sold by both companies
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Medicine> GetTotalMostSold()
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                command.CommandText = @"SELECT M.ID_Medicamento,M.Nombre,SUM(MPS.CantidadVentas) AS CantidadVentas " +
+                                       "FROM Medicamento M  JOIN Medicamentos_Por_Sucursal MPS ON M.ID_Medicamento= MPS.ID_Medicamento JOIN Sucursal S ON MPS.ID_Sucursal=S.ID_Sucursal " +
+                                       "GROUP BY M.ID_Medicamento,M.Nombre ORDER BY CantidadVentas DESC";
+                var result = ToStatisticMedicineList(command);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Get a list of the online most sold medicines for the given company
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Medicine> GetOnlineMostSoldByCompany(string company)
+        {
+            using (var command = Context.CreateDbCommand())
+            {
+                command.CommandText = @"SELECT M.ID_Medicamento,M.Nombre,SUM(MPS.CantidadVentas) AS CantidadVentas "+
+                                       "FROM Medicamento M  JOIN Medicamentos_Por_Sucursal MPS ON M.ID_Medicamento= MPS.ID_Medicamento JOIN Sucursal S ON MPS.ID_Sucursal=S.ID_Sucursal "+
+                                       "JOIN Medicamentos_Por_Pedido MPP ON M.ID_Medicamento=MPP.ID_Medicamento JOIN Pedido P ON P.NumeroPedido=MPP.NumeroPedido "+
+                                       "WHERE p.Tipo_Pedido=1 AND S.Compañia=@company" +
+                                       "GROUP BY M.ID_Medicamento,M.Nombre ORDER BY CantidadVentas DESC;";
+                var newParameter = command.CreateParameter();
+                newParameter.ParameterName = "@company";
+                newParameter.Value = company;
+                command.Parameters.Add(newParameter);
+                var result = ToStatisticMedicineList(command);
                 return result;
             }
         }
@@ -115,9 +154,11 @@ namespace dual_farma.DAL.Repositories
             int ammount = 0;
             using (var command = Context.CreateDbCommand())
             {
-                command.CommandText = @"SELECT SUM(CantidadVentas) AS Ventas FROM Medicamento WHERE CasaFarmaceutica=@casaFarma";
+                command.CommandText = @"SELECT SUM(MPS.CantidadVentas) AS Ventas "+
+                                       "FROM Medicamento M  JOIN Medicamentos_Por_Sucursal MPS ON M.ID_Medicamento= MPS.ID_Medicamento "+
+                                       "JOIN Sucursal S ON MPS.ID_Sucursal=S.ID_Sucursal WHERE S.Compañia=@company";
                 var newParameter = command.CreateParameter();
-                newParameter.ParameterName = "@casaFarma";
+                newParameter.ParameterName = "@company";
                 newParameter.Value = company;
                 command.Parameters.Add(newParameter);
                 using (var reader = command.ExecuteReader())
@@ -126,7 +167,6 @@ namespace dual_farma.DAL.Repositories
                     {
                         var result = reader["Ventas"];
                         ammount = result == DBNull.Value ? 0 : (int)result;
-
                     }
                 }
             }
@@ -142,12 +182,9 @@ namespace dual_farma.DAL.Repositories
             using (var command = Context.CreateDbCommand())
             {
                 var medicineProps = new object[]
-                 {medicine.Name, medicine.RequiresPrescription, medicine.Price, medicine.OriginOffice, medicine.House,
-                 medicine.Stock, medicine.NumberSold};
-                command.CommandText = @"UPDATE Medicamento SET Nombre=@name, Prescripcion=@reqPresc, Precio=@price, Sucursal_Origen=@originOffice, " +
-                                       "CasaFarmaceutica=@house, CantidadDisponible=@stock, CantidadVentas=@numberSold WHERE ID_Medicamento=@medicineId";
-                var parameterNames = new string[] { "@name", "@reqPresc", "@price", "@originOffice", "@house",
-                                                    "@stock", "@numberSold"};
+                 {medicine.Name, medicine.RequiresPrescription};
+                command.CommandText = @"UPDATE Medicamento SET Nombre=@name, Prescripcion=@reqPresc WHERE ID_Medicamento=@medicineId";
+                var parameterNames = new string[] { "@name", "@reqPresc"};
                 for (var i = 0; i < medicineProps.Length; i++)
                 {
                     var newParameter = command.CreateParameter();
@@ -180,16 +217,58 @@ namespace dual_farma.DAL.Repositories
             }
         }
 
+        protected IEnumerable<Medicine> ToCustomMedicineList(IDbCommand command)
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                List<Medicine> itemList = new List<Medicine>();
+                while (reader.Read())
+                {
+                    var item = new Medicine();
+                    MapCustomMedicine(reader, item);
+                    itemList.Add(item);
+                }
+                return itemList;
+            }
+        }
+
+        protected IEnumerable<Medicine> ToStatisticMedicineList(IDbCommand command)
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                List<Medicine> itemList = new List<Medicine>();
+                while (reader.Read())
+                {
+                    var item = new Medicine();
+                    MapStatisticMedicine(reader, item);
+                    itemList.Add(item);
+                }
+                return itemList;
+            }
+        }
+
         protected override void Map(IDataRecord record, Medicine medicine)
         {
             medicine.MedicineId = (Guid)record["ID_Medicamento"];
             medicine.Name = (string)record["Nombre"];
             medicine.RequiresPrescription = (bool)record["Prescripcion"];
-            medicine.Price = (decimal)record["Precio"];
-            medicine.OriginOffice = (int)record["Sucursal_Origen"];
-            medicine.House = (string)record["CasaFarmaceutica"];
-            medicine.Stock = (int)record["CantidadDisponible"];
-            medicine.NumberSold = (int)record["CantidadVentas"];
+        }
+
+        protected  void MapCustomMedicine(IDataRecord record, Medicine medicine)
+        {
+            medicine.MedicineId = (Guid)record["ID_Medicamento"];
+            medicine.Name = (string)record["Nombre"];
+            medicine.RequiresPrescription = (bool)record["Prescripcion"];
+            medicine.Price = (decimal) record["Precio"];
+            medicine.AmmountSold = (int) record["CantidadVentas"];
+            medicine.Stock = (int) record["CantidadDisponible"];
+        }
+
+        protected void MapStatisticMedicine(IDataRecord record, Medicine medicine)
+        {
+            medicine.MedicineId = (Guid)record["ID_Medicamento"];
+            medicine.Name = (string)record["Nombre"];
+            medicine.AmmountSold = (int)record["CantidadVentas"];
         }
     }
 }
