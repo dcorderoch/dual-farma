491f8f35ee322881ec73c14ae18adaad048f7d25
manu3193 <manu3193@gmail.com>
2016-04-02 01:38:15 -0600

    Added password prop

--- dual-farma/dual-farma/DAL/Models/Client.cs
+++ dual-farma/dual-farma/DAL/Models/Client.cs
@@ -8,6 +8,7 @@ namespace dual_farma.DAL.Models
     public class Client
     {
         public string Id { get; set; }
+        public string Password { get; set; }
         public string Name { get; set; }
         public string LastName1 { get; set; }
         public string LastName2 { get; set; }
