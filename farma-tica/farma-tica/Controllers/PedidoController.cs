0ef13075d9b794b6761c9e3232718c4dd30e80e0
David Cordero Chavarría <dcorderoch215@estudiantec.cr>
2016-04-03 13:28:47 -0600

    add preliminar order methods

--- farma-tica/farma-tica/Controllers/PedidoController.cs
+++ farma-tica/farma-tica/Controllers/PedidoController.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
+using farma_tica.BLL;
+using farma_tica.DAL.Models;
 
 namespace farma_tica.Controllers
 {
@@ -19,9 +21,29 @@ namespace farma_tica.Controllers
 
         // URI from Angular: /home/Pedido/Create
         [HttpPost]
-        public JsonResult Create()
+        public JsonResult Create(Order theOrder)
         {
-            
+            //MUST CHANGE
+            /*
+            public int CreateOrderWithoutPrescription(string clientId, string[] medicinesId, string pickupOfficeId,
+            string prefPhoneNume, DateTime pickUpdDate, bool type)
+            */
+            var om = new OrderManager();
+            //return Json(new ReturnStatus() {StatusCode = om.CreateOrderWithoutPrescription(theOrder.ClientId,theOrder.)})
+            return Json(new ReturnStatus() {StatusCode = 200}, JsonRequestBehavior.AllowGet);
+        }
+
+        // URI from Angular: /home/Pedido/CreateWPrescription
+        public JsonResult CreateWPrescription(Order theOrder)
+        {
+            //MUST CHANGE
+            /*
+            public int CreateOrderWithoutPrescription(string clientId, string[] medicinesId, string pickupOfficeId,
+            string prefPhoneNume, DateTime pickUpdDate, bool type)
+            */
+            var om = new OrderManager();
+            //return Json(new ReturnStatus() {StatusCode = om.CreateOrderWithoutPrescription(theOrder.ClientId,theOrder.)})
+            return Json(new ReturnStatus() { StatusCode = 200 }, JsonRequestBehavior.AllowGet);
         }
         // URI from Angular: /home/Pedido/Update
         // URI from Angular: /home/Pedido/Delete
